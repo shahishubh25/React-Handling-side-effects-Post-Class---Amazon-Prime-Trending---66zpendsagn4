60200e3c79b0ca94764bf8a624a0f1d62ddac06b
Shubham Shahi <shubhamshahi552@gmail.com>
2022-02-24 18:57:45 +0530

    ff

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,90 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-const App = (props) => {
-  let { slides } = props;
+const App = ({ slides }) => {
+  const [index, setIndex] = useState(0);
 
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [disabled, setDisabled] = useState(true);
+  const handlePrev = () => {
+    if (index === 0) {
+      return;
+    }
+    setIndex(index - 1);
+  };
+
+  const handleNext = () => {
+    if (index === slides.length - 1) {
+      return;
+    }
+    setIndex(index + 1);
+  };
+
+  const handleRestart = () => {
+    setIndex(0);
+  };
 
   return (
     <>
-      <div>
-        <h1>{slides[selectedIndex].title}</h1>
-        <p>{slides[selectedIndex].text}</p>
-      </div>
-      <div>
-        <button
-          data-testid="button-prev"
-          disabled={selectedIndex === 0}
-          onClick={() => {
-            setDisabled(false);
-            setSelectedIndex(selectedIndex - 1);
-          }}
-        >
-          Prev
-        </button>
-
-        <button
-          data-testid="button-next"
-          disabled={selectedIndex === slides.length - 1}
-          onClick={() => {
-            setDisabled(false);
-            setSelectedIndex(selectedIndex + 1);
-          }}
-        >
-          Next
-        </button>
-
-        <button
-          data-testid="button-restart"
-          disabled={disabled}
-          onClick={() => {
-            setDisabled(true);
-            setSelectedIndex(0);
-          }}
-        >
-          Restart
-        </button>
+      <div className="App">
+        <div id="slide">
+          <h1 data-testid="title">{slides[index].title}</h1>
+          <p data-testid="text">{slides[index].text}</p>
+          <div id="navigation">
+            {index === 0 ? (
+              <button
+                type="button"
+                disabled
+                data-testid="button-prev"
+                onClick={handlePrev}
+              >
+                Previous
+              </button>
+            ) : (
+              <button
+                type="button"
+                data-testid="button-prev"
+                onClick={handlePrev}
+              >
+                Previous
+              </button>
+            )}
+            {index === 0 ? (
+              <button
+                type="button"
+                disabled
+                data-testid="button-restart"
+                onClick={handleRestart}
+              >
+                Restart
+              </button>
+            ) : (
+              <button
+                type="button"
+                data-testid="button-restart"
+                onClick={handleRestart}
+              >
+                Restart
+              </button>
+            )}
+            {index === slides.length - 1 ? (
+              <button
+                type="button"
+                disabled
+                data-testid="button-next"
+                onClick={handleNext}
+              >
+                Next
+              </button>
+            ) : (
+              <button
+                type="button"
+                data-testid="button-next"
+                onClick={handleNext}
+              >
+                Next
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
